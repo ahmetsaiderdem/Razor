d84e76447baebd3dc2b83e9d620830d770deacf0
erdem <erdeahmet750@gmail.com>
2020-10-21 15:45:32 +0300

    Proje dosyası ekle.

---
+++ Razor/Data/RazorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Razor.Model;
+
+namespace Razor.Data
+{
+    public class RazorContext : DbContext
+    {
+        public RazorContext (DbContextOptions<RazorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Razor.Model.Movie> Movie { get; set; }
+    }
+}
